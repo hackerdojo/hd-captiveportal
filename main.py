267767e91c5515a4ac2d00d1bc14059e3686cbdb
Jeff Lindsay <progrium@gmail.com>
2010-12-26 19:09:09 -0800

    getting it all working again

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from django.utils import simplejson
 from gdata.apps.service import AppsService
 import logging
 import base64
+import urllib
 
 DOMAIN = 'hackerdojo.com'
 
@@ -25,38 +26,52 @@ def dojo(path, force=False):
         memcache.set(path, resp, cache_ttl)
     return resp
 
+class MacAddressMapping(db.Model):
+    address = db.StringProperty()
+    username = db.StringProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
+    
+    @classmethod
+    def get_by_mac(cls, address):
+        return cls.all().filter('address =', address).get()
+
 class MainHandler(webapp.RequestHandler):
-    def get(self, data):
+    def get(self, data):   
         try:
+            data = urllib.unquote(data)
             mac, redirect = base64.b64decode(data).split(',')
             self.response.out.write(template.render('templates/main.html', {
                 'redirect': redirect,
                 'mac': mac,
+                'failure': self.request.get('fail')
             }))
         except:
             self.error(400)
+            self.response.out.write("bad request")
     
     def post(self, data):
         client = AppsService(domain=DOMAIN)
+        data = urllib.unquote(data)
         try:
             client.ClientLogin('%s@%s' % (self.request.get('username'), DOMAIN), self.request.get('password'))
             if not dojo('/users/%s' % self.request.get('username'))['suspended']:
-                memcache.set(self.request.get('mac'), self.request.get('username'))
-                self.redirect(self.request.get('redirect'))
+                m = MacAddressMapping(address=self.request.get('mac'), username=self.request.get('username'))
+                m.put()
+                self.redirect(self.request.get('redirect') or 'http://hackerdojo.com')
             else:
                 raise Exception()
         except:
-            self.redirect('/%s' % data)
+            self.redirect('/%s?fail=1' % data)
 
 
 class MacHandler(webapp.RequestHandler):
     def get(self, mac):
-        user = memcache.get(mac)
-        if not user or dojo('/users/%s' % user)['suspended']:
+        mapping = MacAddressMapping.get_by_mac(mac)
+        if not mapping or dojo('/users/%s' % mapping.username)['suspended']:
             self.error(404)
             self.response.out.write("not found")
         else:
-            self.response.out.write(user)
+            self.response.out.write(mapping.username)
         
 
 def main():
