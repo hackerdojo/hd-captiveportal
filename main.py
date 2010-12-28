9bc8c0bd50a7176b7654332bb4264b8cdb2dce18
Jeff Lindsay <progrium@gmail.com>
2010-12-27 18:13:10 -0800

    added a donate/daypass option, and a simple dev entrypoint

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ MEMBER_UPLOAD = 0
 GUEST_TIMEOUT = 43200 # 12 hours
 #GUEST_TIMEOUT = 60
 GUEST_NAME = 'internet-guest*'
+DAYPASS_NAME = 'internet-daypass*'
 DEFAULT_REDIRECT = 'http://hackerdojo.com'
+DONATE_BUTTON = '6JB7VARJDVHAG'
 
 def dojo(path, force=False):
     """ Hacker Dojo Domain API helper with caching """
@@ -120,7 +122,39 @@ class GuestHandler(webapp.RequestHandler):
     def post(self):
         memcache.set(self.request.get('mac'), GUEST_NAME, time=GUEST_TIMEOUT)
         self.redirect(self.request.get('redirect') or DEFAULT_REDIRECT)
-        
+
+class DonateHandler(webapp.RequestHandler):
+    """ Form handler when donating for a day pass """
+    
+    def post(self):
+        mac = self.request.get('mac')
+        redirect = self.request.get('redirect') or DEFAULT_REDIRECT
+        callback = "%s/donate/%s" % (self.request.host_url, base64.b64encode(','.join([mac, redirect])))
+        memcache.set(mac, GUEST_NAME, time=GUEST_TIMEOUT)
+        self.redirect("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=%s&notify_url=%s&return=%s" %
+            (DONATE_BUTTON, callback, callback))
+
+class DonateCallbackHandler(webapp.RequestHandler):
+    """ Callback handler for PayPal IPN (POST) and return from PayPal (GET) """
+    
+    def post(self, data):
+        data = urllib.unquote(data)
+        mac, redirect = base64.b64decode(data).split(',')
+        if self._validate_ipn() and self.request.get('payment_status').lower() == 'completed':
+            memcache.set(mac, DAYPASS_NAME, time=GUEST_TIMEOUT)
+        self.get(data)
+    
+    def _validate_ipn(self):
+        resp = urlfetch.fetch('https://www.paypal.com/cgi-bin/webscr', method="POST",
+            payload="cmd=_notify-validate&%s" % self.request.body)
+        return "VERIFIED" in resp.content
+    
+    def get(self, data):
+        data = urllib.unquote(data)
+        mac, redirect = base64.b64decode(data).split(',')
+        if not redirect.startswith('http'):
+            redirect = DEFAULT_REDIRECT
+        self.response.out.write(template.render('templates/daypass.html', {'hours': GUEST_TIMEOUT/3600, 'redirect': redirect}))
 
 class MacHandler(webapp.RequestHandler):
     """ Endpoint used by RADIUS bridge """
@@ -128,7 +162,10 @@ class MacHandler(webapp.RequestHandler):
     def get(self, mac):
         guest = memcache.get(mac)
         if guest:
-            self.response.out.write("%s,," % guest)
+            if guest == DAYPASS_NAME:
+                self.response.out.write("%s,%s,%s" % (guest, MEMBER_DOWNLOAD, MEMBER_UPLOAD))
+            else:
+                self.response.out.write("%s,," % guest)
             touch_stat('guests')
         else:
             mapping = MacAddressMapping.get_by_mac(mac)
@@ -151,12 +188,25 @@ class StatHandler(webapp.RequestHandler):
             stat = members+guests
         self.response.out.write(str(stat))
 
+class DevHandler(webapp.RequestHandler):
+    """ Convenience entry point for local development """
+    
+    def get(self):
+        self.response.out.write(template.render('templates/dev.html', {}))
+    
+    def post(self):
+        self.redirect('/%s' % base64.b64encode(','.join([
+            self.request.get('mac'), 
+            self.request.get('redirect')])))
 
 def main():
     application = webapp.WSGIApplication([
+        ('/dev', DevHandler),
         ('/api/mac/(.+)', MacHandler),
         ('/api/stat/(.+)', StatHandler),
         ('/guest', GuestHandler),
+        ('/donate/(.+)', DonateCallbackHandler),
+        ('/donate', DonateHandler),
         ('/member', MemberHandler),
         ('/(.+)', EntryHandler),] ,debug=True)
     util.run_wsgi_app(application)
